17867b1c4310d754a3970381ec9006ce62955637
zhangyuwinnie <zhangyuwinnie@tamu.edu>
2017-04-02 01:25:35 -0500

    solution to LC144, recursive and iterative

---
+++ src/LC144.java
@@ -0,0 +1,37 @@
+// Medium
+// Given a binary tree, return the preorder traversal of its nodes' values.
+
+// For example:
+// Given binary tree {1,#,2,3},return [1,2,3].
+
+// recursive solution, less efficient
+import java.util.List;
+import java.util.ArrayList;
+public class LC144 {
+	public class TreeNode {
+		 int val;
+		 TreeNode left;
+		 TreeNode right;
+		 TreeNode(int x) { val = x; }
+	}
+	
+	public List<Integer> preorderTraversal(TreeNode root) {
+	     List<Integer> result = new ArrayList<Integer>();
+	     if (root == null){
+	    	 return result;
+	     }
+	     helper(root, result);
+	     return result;
+	}
+	public void helper(TreeNode root, List<Integer> result){
+		if (root == null) return;
+		result.add(root.val);
+		if (root.left != null){
+			helper(root.left, result);
+		}
+		if(root.right != null){
+			helper(root.right, result);
+		}
+	}
+	
+}
---
+++ src/LC144_2.java
@@ -0,0 +1,40 @@
+// Medium
+// Given a binary tree, return the preorder traversal of its nodes' values.
+
+// For example:
+// Given binary tree {1,#,2,3},return [1,2,3].
+
+// iterative solution
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+public class LC144_2 {
+	public class TreeNode {
+		 int val;
+		 TreeNode left;
+		 TreeNode right;
+		 TreeNode(int x) { val = x; }
+	}
+	
+	public List<Integer> preorderTraversal(TreeNode root) {
+	     List<Integer> result = new ArrayList<Integer>();
+	     if (root == null){
+	    	 return result;
+	     }
+	     Stack<TreeNode> s = new Stack<TreeNode>();
+	     s.push(root);
+	     while (!s.empty()  ){
+	    	 root = s.pop();
+	    	 result.add(root.val);
+	    	 if (root.right != null){
+	    		 s.push(root.right);
+	    	 }
+	    	 if(root.left != null){
+	    		 s.push(root.left);
+	    	 }
+	     }
+	     return result;
+	}
+	
+	
+}
