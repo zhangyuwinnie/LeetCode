44e689fc1384a6e26a8ab708aa9c3fa8ade41607
zhangyuwinnie <zhangyuwinnie@tamu.edu>
2017-04-04 18:30:15 -0500

    LC141

---
+++ src/LC141.java
@@ -0,0 +1,32 @@
+/**
+ * Given a linked list, determine if it has a cycle in it.
+ * Follow up:
+ * Can you solve it without using extra space?
+ * @author zy
+ *
+ */
+
+public class LC141 {
+	
+	class ListNode{
+		int val;
+		ListNode next;
+		ListNode(int x){
+			val = x;
+			next = null;
+		}
+	}
+	
+	public boolean hasCycle(ListNode head) {
+        if (head == null){
+       	 	return false;
+        }
+        while (head.next != null && head.next != head){
+       	 	ListNode nextNode = head.next;
+       	 	head.next = nextNode.next;
+       	 	head = nextNode;
+       	 	
+        }
+        return head.next != null;
+   }
+}
