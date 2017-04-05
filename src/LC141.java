66babfbcf4a42465c9b07fca8df81a3a54641533
zhangyuwinnie <zhangyuwinnie@tamu.edu>
2017-04-05 14:55:59 -0500

    add solution 2 to LC 141

--- src/LC141.java
+++ src/LC141.java
@@ -7,7 +7,7 @@
  */
 
 public class LC141 {
-
+	//solution1
 	public boolean hasCycle(ListNode head) {
         if (head == null){
        	 	return false;
@@ -20,8 +20,36 @@ public class LC141 {
         }
         return head.next != null;
     }
+	// solution2
+	public boolean hasCycle2(ListNode head){
+		if (head == null){
+			return false;
+		}
+		ListNode slow = head;
+		ListNode quick = head;
+		while(slow.next!=null && quick.next!=null&&quick.next.next!=null){
+			slow = slow.next;
+			quick = quick.next.next;
+			if (slow == quick){
+				return true;
+			}
+		}
+		return false;
+	}
 	
 	public static void main (String[] args){
+		ListNode head = new ListNode(0);
+		ListNode head1 = new ListNode(1);
+		ListNode head2 = new ListNode(2);
+		ListNode head3 = new ListNode(3);
+		head.next = head1;
+		head1.next = head2;
+		head2.next = head3;
+		head3.next = head1;
+		
+		LC141 test = new LC141();
+		System.out.println(test.hasCycle2(head));
+		
 		
 	}
 }
