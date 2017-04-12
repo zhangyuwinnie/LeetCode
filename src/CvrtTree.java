79886fe401bd8f8e1780dd3045f767b99b92629d
zhangyuwinnie <zhangyuwinnie@tamu.edu>
2017-04-12 00:12:49 -0500

    add tree2array method and modify problem file

--- src/CvrtTree.java
+++ src/CvrtTree.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * Class for converting int array into a binary tree
  * Usage: CvrtTree test = new CvrtTree();
@@ -13,6 +19,37 @@ public class CvrtTree {
 			return null;
 		}
 		return constructTree(array, 1);
+	}
+	
+	public int[] tree2Array(TreeNode root){
+		
+			 List<Integer> result = new ArrayList<Integer>();
+			 if (root == null) return null;
+			 // TreeNode  curr = root;
+			 Queue<TreeNode> q = new LinkedList<TreeNode>();
+			 q.add(root);
+			 while (!q.isEmpty()){
+				 int size = q.size();
+				 // List<Integer> level = new ArrayList<Integer>();
+				 for ( int i = 0; i<size; i++){
+					 TreeNode curr = q.poll();
+					 result.add(curr.val);
+					 if (curr.left != null){
+						 q.add(curr.left);
+					 }
+					 if (curr.right != null){
+						 q.add(curr.right);
+					 }
+				 }
+				 // result.add(level);
+			 }	
+			 int[] arr = new int[result.size()];
+			 int index = 0;
+			 for (int i = 0; i < result.size(); i++){
+				 arr[i] = result.get(index++);
+			 }
+			 return arr;
+	    
 	}
 	
 	public TreeNode constructTree(int[] input, int index){
@@ -57,6 +94,8 @@ public class CvrtTree {
 		CvrtTree test = new CvrtTree();
 		int[] a = {2, 1, 4, -1, -1, 3, 5};
 		TreeNode tree = test.constructTree(a, 1);
+		int[] result = test.tree2Array(tree);
+		System.out.println(Arrays.toString(result));
 		System.out.println("Inorder traversal of the tree is:");
 	    test.inorder(tree);
 	    System.out.println("\nPreorder traversal of the tree is:");
