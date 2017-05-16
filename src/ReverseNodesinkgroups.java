86284a1ce030ced29c252337f5f528eb342366e6
zhangyuwinnie <zhangyuwinnie@tamu.edu>
2017-05-16 11:20:39 -0500

    Modify reverse linkedlist in k groups

--- src/ReverseNodesinkgroups.java
+++ src/ReverseNodesinkgroups.java
@@ -1,60 +1,55 @@
 
 public class ReverseNodesinkgroups {
-	public class Solution {
-	    /**
-	     * @param head a ListNode
-	     * @param k an integer
-	     * @return a ListNode
-	     */
-	    public ListNode reverseKGroup(ListNode head, int k) {
-	        ListNode dummy = new ListNode(0);
-	        dummy.next = head;
-	        
-	        head = dummy;
-	        while (true) {
-	            head = reverseK(head, k);
-	            if (head == null) {
-	                break;
-	            }
-	        }
-	        
-	        return dummy.next;
-	    }
-	    
-	    // head -> n1 -> n2 ... nk -> nk+1
-	    // =>
-	    // head -> nk -> nk-1 .. n1 -> nk+1
-	    // return n1
-	    public ListNode reverseK(ListNode head, int k) {
-	        ListNode nk = head;
-	        for (int i = 0; i < k; i++) {
-	            if (nk == null) {
-	                return null;
-	            }
-	            nk = nk.next;
-	        }
-	        
-	        if (nk == null) {
-	            return null;
-	        }
-	        
-	        // reverse        
-	        ListNode n1 = head.next;
-	        ListNode nkplus = nk.next; 
-	        
-	        ListNode prev = null;
-	        ListNode curt = n1;
-	        while (curt != nkplus) {
-	            ListNode temp = curt.next;
-	            curt.next = prev;
-	            prev = curt;
-	            curt = temp;
-	        }
-	        
-	        // connect
-	        head.next = nk;
-	        n1.next = nkplus;
-	        return n1;
-	    }
-	}
+    /**
+     * @param head a ListNode
+     * @param k an integer
+     * @return a ListNode
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null){
+            return null;
+        }
+        if (k < 0){
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        while (prev != null){
+            prev = reverseKNodes(prev, k);
+        }
+        return dummy.next;
+    }
+    // prev-n1-..nk-nk+1
+    // prev-nk-..n1-nk+1
+    public ListNode reverseKNodes(ListNode prev, int k){
+        ListNode n1 = prev.next;//n1-1
+        ListNode nk = prev;//nk-0
+        for (int i = 0; i < k; i++){
+            nk = nk.next;
+            if (nk == null){
+                return null;
+            }
+        }
+        ListNode nkplus = nk.next;
+        // reverse
+        ListNode preCurt = prev;
+        ListNode curt = prev.next;
+        for (int i = 0; i < k; i++){
+            ListNode temp = curt.next;
+            curt.next = preCurt;
+            preCurt = curt;
+            curt = temp;
+        }
+        //重新连接
+        n1.next = nkplus; //!!!
+        prev.next = preCurt; //!!!
+        // ListNode traverse = prev;
+        // while (traverse != null) {
+        //     System.out.println(traverse.val);
+        //     traverse = traverse.next;
+            
+        // }
+        return n1;
+    }
 }
