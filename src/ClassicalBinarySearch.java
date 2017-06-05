d5199d78f70e612c4f6319a366eec2b59fd27c6c
zhangyuwinnie <zhangyuwinnie@tamu.edu>
2017-06-05 16:59:24 +0800

    Binary Search and first/last option

---
+++ src/ClassicalBinarySearch.java
@@ -0,0 +1,38 @@
+
+public class ClassicalBinarySearch {
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return an integer
+     */
+    public int findPosition(int[] nums, int target) {
+        // Write your code here
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start)/2;
+            if (nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target){
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (nums[start] == target){
+            return start;
+        }
+        
+        if (nums[end] == target){
+            return end;
+        }
+        
+        return -1;
+        
+        
+    }
+}
