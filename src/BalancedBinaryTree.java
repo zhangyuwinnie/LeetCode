b76bb7ebe1a7cd36d0aace890e817d1dfb4cd216
zhangyuwinnie <zhangyuwinnie@tamu.edu>
2017-07-09 14:13:20 +0800

    Add another solution to Balanced Binary Tree

--- src/BalancedBinaryTree.java
+++ src/BalancedBinaryTree.java
@@ -35,4 +35,30 @@ public class BalancedBinaryTree {
 	    }
 	    return new ResultType(true, Math.max(helper(root.left).maxDepth, helper(root.right).maxDepth) + 1);
 	}
+	
+//	自创方法：用global变量和helper函数分别负责追踪一个特性，这样就可以追踪两个变量
+	public class Solution {
+	    /**
+	     * @param root: The root of binary tree.
+	     * @return: True if this Binary tree is Balanced, or false.
+	     */
+	    private boolean balanced = true;
+	    public boolean isBalanced(TreeNode root) {
+	        // write your code here
+	        helper(root);
+	        return balanced;
+	    }
+	    
+	    public int helper(TreeNode root){//1
+	        if (root == null){
+	            return 0;
+	        }
+	        int left = helper(root.left);
+	        int right = helper(root.right);
+	        if (Math.abs(left - right) > 1){
+	            balanced = false;
+	        }
+	        return Math.max(left, right) + 1;
+	    }
+	}
 }
